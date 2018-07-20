d76ba21c6670b9c67fc55f06239bf14a030033e0
Ranjan <ranjan@accessibleict.com>
2018-07-20 18:24:42 +0545

    added initials angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { UserappComponent } from './userapp/userapp.component';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserappComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/userapp/userapp.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userapp',
+  templateUrl: './userapp.component.html',
+  styleUrls: ['./userapp.component.css']
+})
+export class UserappComponent implements OnInit {
+	title = "My Custom App";
+	currentRate = 8;
+
+	uname: string;
+	uage: number;
+	uphone: number;
+	formArray = [];
+	formbinding() {
+		let formobj = {
+			username : this.uname,
+			userage : this.uage,
+			userphone : this.uphone,
+			urate : this.currentRate
+		}
+		this.formArray.push(formobj)
+	}
+
+	delete(i) {
+		this.formArray.splice(i,1)
+	}
+
+	edit(i) {
+		let useredit = this.formArray[i]
+		this.uname = useredit['username']
+		this.uage = useredit['userage']
+	}
+
+  constructor() { }
+
+  ngOnInit() {
+  	// this.formbinding();
+  }
+
+}
