693ef0d3d549226fcb68ecb876f4ca007850f189
ranjan <wanbblack68@gmail.com>
2018-07-23 21:52:09 +0545

    Monday class files

--- src/app/userapp/userapp.component.ts
+++ src/app/userapp/userapp.component.ts
@@ -31,6 +31,8 @@ export class UserappComponent implements OnInit {
 		let useredit = this.formArray[i]
 		this.uname = useredit['username']
 		this.uage = useredit['userage']
+		this.uphone = useredit['userphone']
+		this.currentRate = useredit['urate']
 	}
 
   constructor() { }
